44bf1c5ee361254be5d4f79ab09e610d51d8af53
Harrisam <samantha.s.harris@googlemail.com>
2013-09-18 21:23:05 +0100

    final touchs to fix bugs

--- sudoku.rb
+++ sudoku.rb
@@ -23,7 +23,7 @@ end
 #this method removes some digits from the solution to create a puzzle
 def puzzle(sudoku)
 	# this method is yours to implement
-	random = (0..81).to_a.sample(15)
+	random = (0..81).to_a.sample(25)
 	@puzzled = []
 	sudoku.each_with_index do |element,index|
 		if random.include?(index) then @puzzled.push(0)
@@ -46,6 +46,20 @@ def prepare_to_check_solution
 		flash[:notice] = "Incorrect values are highlighted in purple"
 	end
 	session[:check_solution] = nil
+end
+
+def box_order_to_row_order(cells)
+	boxes = cell.each_slice(9).to_a
+	(0..8).to_a.inject([]) do |memo, i|
+		first_box_index = i / 3 * 3
+		three_boxes = boxes[first_box_index, 3]
+		three_rows_of_three = three_boxes.map do |box| 
+			row_number_in_a_box = i % 3
+			first_cell_in_the_row_index = row_number_in_a_box * 3
+			box[first_cell_in_the_row_index, 3]
+		end
+		memo += three_rows_of_three.flatten
+	end
 end
 
 get '/random' do 
